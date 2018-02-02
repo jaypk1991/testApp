32954010696540cabb990e026c6de68efdf38c39
Davide Giacalone <Davide.Giacalone@ats-global.com>
2018-02-02 10:58:15 +0100

    Initial commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require("http");
 http.createServer( (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end("<h1>Greetings from Bill Gates!</h1>");
-}).listen(80);
+}).listen(4200);
 console.log("Server running at 'webappia.azurewebsites.net'...");
 
