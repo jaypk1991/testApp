c8f10b40ab6f61ab8a87fd9d53a3381fe19b512d
Davide Giacalone <Davide.Giacalone@ats-global.com>
2018-02-02 11:19:27 +0100

    changed: testing response 404

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require("http");
 
 http.createServer( (request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end("<h1>Greetings from Bill Gates!</h1>");
+    // response.writeHead(200, {'Content-Type': 'text/html'});
+    // response.end("<h1>Greetings from Bill Gates!</h1>");
+    respons.writeHead(404);
 }).listen(80);
 console.log("Server running at 'webappia.azurewebsites.net'...");
 
